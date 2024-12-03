43014a09a630bd2e6b690ebd39261cc5b9a6d64d
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2024-12-03 15:18:15 +0900

    [Fix] User Name Is Unique

--- user/src/main/kotlin/com/commitAttack/be/user/domain/UserRepository.kt
+++ user/src/main/kotlin/com/commitAttack/be/user/domain/UserRepository.kt
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<User, UUID> {
-    fun findByGithubIdAndDeletedAtIsNull(githubId: String): User?
+    fun findByNameAndDeletedAtIsNull(name: String): User?
     fun findByIdAndDeletedAtIsNull(id: String) : User?
 }
